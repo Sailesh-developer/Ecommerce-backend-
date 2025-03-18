36600f76080e5db84ff488486a7d19ae3cac934f
Sailesh-developer <sailesh.developer2001@gmail.com>
2025-03-18 16:59:51 +0530

    connecting loose ends

--- src/main/java/com/ecommerce/project/model/Address.java
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,6 +47,7 @@ public class Address {
     @Size(min = 6, message = "Pincode must have atleast 6 characters.")
     private String pincode;
 
+    @ToString.Exclude
     @ManyToMany(mappedBy = "addresses")
     private List<User> users = new ArrayList<>();
 
